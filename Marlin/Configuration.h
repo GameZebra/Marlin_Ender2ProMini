9786b0eca5508559aa154ff51f30bc1ac26de030
GameZebra <mr.xmitkotox@gmail.com>
2025-03-10 12:29:41 +0200

    autohome dimensions fixed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1891,9 +1891,9 @@
 #define Y_BED_SIZE 165
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -50
-#define Y_MIN_POS -50
-#define Z_MIN_POS -50
+#define X_MIN_POS -18
+#define Y_MIN_POS -5
+#define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 180
